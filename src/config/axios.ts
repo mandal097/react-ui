64d68c84865d4ace57ad176c17aa095a999ed320
Amarnath kumar mandal <amarnathmandal097gmail.com>
2024-08-30 17:37:50 +0530

    wip

--- src/config/axios.ts
+++ src/config/axios.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 // const apiHost = 'http://localhost:5000/api/v1/';
-const apiHost = 'https://react-ui-api.onrender.com/:5000/api/v1/';
+const apiHost = 'https://react-ui-api.onrender.com/api/v1/';
 
 const ax = axios.create({
   baseURL: apiHost,
