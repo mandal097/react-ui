4b558ce81b9aee107401c9726a4b282e076d6e2e
Amarnath kumar mandal <amarnathmandal097gmail.com>
2024-08-30 17:34:26 +0530

    fixes issues

--- src/config/axios.ts
+++ src/config/axios.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 // const apiHost = 'http://localhost:5000/api/v1/';
-const apiHost = 'http://192.168.1.3:5000/api/v1/';
+const apiHost = 'https://react-ui-api.onrender.com/:5000/api/v1/';
 
 const ax = axios.create({
   baseURL: apiHost,
